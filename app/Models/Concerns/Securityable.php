9976fb38a1fe513efec13faafa20a4deca2fddc2
D-Jordy <kaaljordy@gmail.com>
2025-11-28 13:06:04 +0100

    Add Security, Stock and Securityable

---
+++ app/Models/Concerns/Securityable.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models\Concerns;
+
+use App\Models\Security;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\MorphOne;
+
+abstract class Securityable extends Model
+{
+    protected $with = ['security'];
+
+    public function security(): MorphOne
+    {
+        return $this->morphOne(Security::class, 'securityable');
+    }
+
+    public function getAttribute($key)
+    {
+        $attribute = parent::getAttribute($key);
+
+        if ($attribute !== null) {
+            return $attribute;
+        }
+
+        $this->loadMissing('security');
+
+        if ($this->relationLoaded('security') && $this->security) {
+            return $this->security->getAttribute($key);
+        }
+
+        return null;
+    }
+}
