61880f58ead56d431a38a0e61514cd8c994ce3a7
D-Jordy <kaaljordy@gmail.com>
2025-12-03 14:55:03 +0100

    return type

--- app/Http/Controllers/PortfolioController.php
+++ app/Http/Controllers/PortfolioController.php
@@ -4,11 +4,10 @@ namespace App\Http\Controllers;
 
 use App\DTO\PortfolioDTO;
 use App\Models\Portfolio;
-use Illuminate\Http\Request;
 
 class PortfolioController extends Controller
 {
-    public function show()
+    public function show(): PortfolioDTO
     {
         return PortfolioDTO::fromModel(
             Portfolio::where('user_id', auth()->id())->firstOrFail()
