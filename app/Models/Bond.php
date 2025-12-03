2f56697933ff2f854315259e9a886959bdc3210e
D-Jordy <kaaljordy@gmail.com>
2025-12-03 08:56:55 +0100

    Add BondTest

--- app/Models/Bond.php
+++ app/Models/Bond.php
@@ -8,4 +8,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 class Bond extends Securityable
 {
     use HasFactory;
+
+    protected $casts = [
+        'maturity_date' => 'immutable_date',
+    ];
 }
