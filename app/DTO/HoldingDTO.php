cfa01059cadc43cdc868acf6978671a13cd9002d
D-Jordy <91130457+D-Jordy@users.noreply.github.com>
2025-12-08 08:29:08 +0000

    style: apply Laravel Pint fixes

--- app/DTO/HoldingDTO.php
+++ app/DTO/HoldingDTO.php
@@ -5,10 +5,15 @@ namespace App\DTO;
 class HoldingDTO extends DTO
 {
     public int $id;
+
     public string $ticker;
+
     public int $quantity;
+
     public float $purchase_price;
+
     public float $avg_price;
+
     public float $gain_loss;
 
     public static function fromModel(object $model): HoldingDTO
