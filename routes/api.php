6e39501bb42bfaa480af83e83570a0d48888ad84
Jacky Liu <liukcj.dev@gmail.com>
2025-12-08 20:39:21 +0100

    Fixed the auth issue in the SanctumTest.php

--- routes/api.php
+++ routes/api.php
@@ -9,10 +9,10 @@ Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::post('/logout', [AuthController::class, 'logout']);
-    Route::get('/user', function (Request $request) {
-        return $request->user();
+    Route::get('/user', function () {
+        return Auth()->user();
     });
-  
+
     Route::controller(PortfolioController::class)->group(function () {
         Route::get('/portfolio', 'show')->name('portfolio.show');
     });
