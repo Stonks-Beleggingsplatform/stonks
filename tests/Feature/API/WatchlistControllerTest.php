72b235248b99b7e7709f7d9eb8bf314d9353db47
D-Jordy <kaaljordy@gmail.com>
2025-12-09 14:13:28 +0100

    Use UserDTO type in WatchlistDTO

--- tests/Feature/API/WatchlistControllerTest.php
+++ tests/Feature/API/WatchlistControllerTest.php
@@ -10,5 +10,15 @@ test('create', function () {
     $response = $this->postJson(route('watchlist.create'), $data);
 
     expect($response->status())->toBe(201)
-        ->and($response->json())->toEqual(WatchlistDTO::make($this->user->watchlists()->latest()->first())->toArray());
+        ->and($response->json())->toEqual(
+            WatchlistDTO::make($this->user->watchlists()->latest()->first())
+                ->jsonSerialize()
+        );
+});
+
+test('create validation', function () {
+    $response = $this->postJson(route('watchlist.create'), []);
+
+    expect($response->status())->toBe(422)
+        ->and($response->json('errors'))->toHaveKey('name');
 });
