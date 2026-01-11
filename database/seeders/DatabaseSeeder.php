710817d199b08a856f8348d7ff8483bcbfbac4f0
Jacky Liu <liukcj.dev@gmail.com>
2026-01-11 14:57:46 +0100

    typescript:transform gerunt en dead code verwijdert uit de seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,22 +16,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
         $this->call([
             AdminUserSeeder::class,
             RegularUserSeeder::class,
             StandardSeeder::class,
         ]);
 
-        /*
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-            'password' => bcrypt('password'),
-        ]);
-        */
-      
         User::factory()
             ->has(
                 Watchlist::factory(3)
